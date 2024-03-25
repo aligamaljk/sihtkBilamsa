5a0f966a28890085998c31714c02a0d1f32b3d34
Mahmoud Mohsen <mahmoud.mohsen120@gmail.com>
2024-03-25 21:13:39 +0200

    Adding the About Us page: Description info about the website, and some images" added also some properties in the ar.tsx and en.tsx files in the localization, and added some types in the index.tsx file in the types folder

---
+++ src/Components/AboutUs/AboutUs.tsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom';
+import { ITranslation } from '../../types';
+import { IoIosArrowForward } from 'react-icons/io';
+import './AboutUs.scss';
+
+function AboutUs({ t }: ITranslation) {
+  return (
+    <div className='about'>
+      {/* About Page Header */}
+      <div className='about-us-header'>
+        <div className='bg-image'></div>
+        <div className='section-header'>
+          <h1 className='title'>{t.aboutUs}</h1>
+          <div className='link'>
+            <Link to='/'>{t.home}</Link>
+            <IoIosArrowForward />
+            {t.contactUs}
+          </div>
+        </div>
+      </div>
+
+      {/* About Page Body */}
+      <div className='about-us-body'>
+        <div className='description-section'>
+          <div className='des-sec-images'>
+            <div className='rectangle one'>
+              <div className='overlay'>
+                <img src='about-1.jpg' alt='athletes doing sports' />
+              </div>
+              <div className='overlay'>
+                <img src='about-2.jpg' alt='fitness tools' />
+              </div>
+            </div>
+            <div className='rectangle two'>
+              <div className='overlay'>
+                <img src='about-3.svg' alt='sports tools' />
+              </div>
+
+              <div className='overlay'>
+                <img
+                  src='about-4.svg'
+                  alt='athlete running above a phone'
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className='des-sec-info'>
+            <h2>
+              <span>{t.aboutPage?.about}</span>
+              <span>{t.websiteName}</span>
+            </h2>
+            <div className='text'>
+              <p>{t.aboutPage?.aboutDescriptionP1}</p>
+              <p>{t.aboutPage?.aboutDescriptionP2}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
